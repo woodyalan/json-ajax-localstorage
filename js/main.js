d4dd5bc52fb0d78560b381dbd69fa4162464565b
Alan Santos <alansantos@grupointegrado.br>
2020-09-14 22:48:40 -0300

    Exemplos dados em aula

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Recebendo os dados no formato JSON
+$(document).ready(function () {
+  $.ajax({
+    url: '../json/pessoas.json',
+    // xml, html, script, text
+    dataType: 'json',
+    type: 'GET',
+    success: function (result) {
+      for (const pessoa of result) {
+        console.log(pessoa.nome);
+      }
+    },
+  });
+
+  let pessoa = {
+    ativo: true,
+    foto: 'http://placehold.it/32x32',
+    idade: 38,
+    nome: 'Howard Miles',
+    email: 'howardmiles@remold.com',
+    telefone: '+1 (896) 488-2805',
+    endereco: {
+      logradouro: 'Rua Araruna',
+      numero: '123',
+      Bairro: 'Centro',
+      cep: '87305-553',
+    },
+  };
+
+  // Enviando os dados no formato JSON
+  $.ajax({
+    url: 'http://localhost:3000/pessoa',
+    // xml, html, script, text
+    dataType: 'json',
+    type: 'POST',
+    data: JSON.stringify(pessoa),
+    contentType: 'application/json',
+    success: function (result) {
+      console.log(result);
+    },
+  });
+});
